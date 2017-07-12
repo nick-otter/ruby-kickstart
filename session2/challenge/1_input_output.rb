576da83da7f8fdf6f247893c562942b6489bbe03
Nick Otter <nicholas.a.otter@gmail.com>
2017-07-12 15:58:22 +0100

    Added challenge 2:1 1st attempt

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -3,10 +3,17 @@
 
 # Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
-# 
+#
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
-def sum_difference_product
+def sum_difference_product(number = gets.chomp)
   # your code goes here
+  arr = number.split('')
+  arr_nospace = arr.map {|x| Integer(x) rescue nil}.compact
+
+  puts arr_nospace[0] + arr_nospace[1]
+  puts arr_nospace[0] - arr_nospace[1]
+  puts arr_nospace[0] * arr_nospace[1]
+
 end
