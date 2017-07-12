eecb86a66246a7c39b7552e62385c92cc2d03d4b
Nick Otter <nicholas.a.otter@gmail.com>
2017-07-12 12:06:06 +0100

    Added challenge 1:7 1st attempt

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,16 @@
 # given a string, return the character after every letter "r"
-# 
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  #sort through letters in a string, return next letter
+  array = string.split('')
+  array.select.each_with_index do |item, index|
+    if item == "r" 
+      array[index + 1].join
+    else
+      nil
+    end
+  end
 end
