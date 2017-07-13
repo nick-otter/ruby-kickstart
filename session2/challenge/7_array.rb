83d54602391b61303ae1c938d130c88c3b1a11b1
Nick Otter <nicholas.a.otter@gmail.com>
2017-07-13 12:19:57 +0100

    Added challenge 2:7

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,15 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(str)
+  result = []
+  clean_arr = str.gsub(/[^'’A-Za-z0-9\s]/i, " ").split
+  clean_arr.each_with_index { |item, index| result << item if index % 2 == 0 }
+  result
+end
