525a094fd458ceb6ebf8725e3b5cea08009cba61
Nick Otter <nicholas.a.otter@gmail.com>
2017-07-14 12:57:18 +0100

    Added challenge 2:10

--- session2/challenge/10_classes.rb
+++ session2/challenge/10_classes.rb
@@ -14,6 +14,20 @@
 # josh.birthday # => 30
 # josh.age      # => 30
 #
+#Person.new makes a new instance of the class Person, you are instantiating an object of the class Person
 
 class Person
+  #attr_accessor is a method that writes methods, named after whatever parameter it receives
+  attr_accessor :name, :age # => [:name (setter), :miles=(getter)]
+
+  #initlalize the name and objects, initialize is a special method, will always be called on a new class object and pass all the arguments you passed onto Person.new onto the method initalize
+  #setter method
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def birthday
+    @age += 1
+  end
 end
