eda17ee03685c54eed00aa34af6301f48fc629e6
Nick Otter <nicholas.a.otter@gmail.com>
2017-07-17 13:59:54 +0100

    Added challenge 3:3 2nd attempt solved

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -7,14 +7,23 @@
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
 
-#WHITE FLAG MESSAGE
 
 def word_count(string)
-  hash = {}
-  downcase_words = string.split.select {|word| word == word.downcase}
-
-  downcase_words.each do |word|
-    hash[word] = downcasewords.select {|n| n == word}.length
-  end
- hash
+    # new hash created with default value of 0
+    hash = Hash.new(0)
+    words = string.split(" ")
+    #iterates over chars, adds +=1 values to hash downcase
+    words.each {|w| hash[w.downcase] += 1}
+    return hash
 end
+
+
+#def word_count(string)
+#  hash = {}
+#  downcase_words = string.split.select {|word| word == word.downcase}
+
+#  downcase_words.each do |word|
+#    hash[word] = downcase_words.select {|n| n == word}.length
+#  end
+# hash
+#end
