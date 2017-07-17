5f0d555eec0515825ca20012e18896091dd853c2
Nick Otter <nicholas.a.otter@gmail.com>
2017-07-17 13:49:38 +0100

    Added challenge 3:2 2nd attempt solved

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,20 +8,30 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
-staircase(num)
+def staircase(num)
 
-hash = Hash.new([])
-
-num.times do |n|
-  n += 1
-  hash[n] = [] if n.odd?
-end
-
-hash.each do |k, v|
-  array =[]
-  1.upto(k) { |x| array.push(x) if x.even? && x < k }
-  hash[k] = array
-end
-
+hash = Hash.new
+  #iterating from 1 in the array up to (num), iterating over size
+  1.upto num do |size|
+  #skip over even numbers
+  next if size.even?
+    arr = Array.new(size) { |i| i + 1 }
+    even_arr = arr.select {|i| i.even?}
+    hash[size] = even_arr
+  end
   hash
 end
+
+#num.times do |n|
+#  n += 1
+#  hash[n] = [] if n.odd?
+#end
+
+#hash.each do |k, v|
+#  array =[]
+#  1.upto(k) { |x| array.push(x) if x.even? && x < k }
+#  hash[k] = array
+#end
+
+#  hash
+#end
